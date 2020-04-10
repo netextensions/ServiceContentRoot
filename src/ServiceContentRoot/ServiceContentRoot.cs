aad72c7bf13d07a5908722ad4a7e407ec2b069cb
Csaba Salat <csalat@globalblue.com>
2020-04-10 17:02:34 +0200

    Servce content root

---
+++ src/ServiceContentRoot/ServiceContentRoot.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Diagnostics;
+using System.IO;
+ 
+namespace ServiceContentRoot
+{
+    public class ServiceContentRoot
+    {
+        public string GetContentRoot() => Process.GetCurrentProcess().MainModule != null ? Path.GetDirectoryName(Process.GetCurrentProcess().MainModule.FileName) : throw new ApplicationException("processModule is not found");
+    }
+}
