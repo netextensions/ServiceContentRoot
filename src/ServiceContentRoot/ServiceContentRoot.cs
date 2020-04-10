4efd477d708c0e72a440378800c21359833f74d5
Csaba Salat <csalat@globalblue.com>
2020-04-10 21:24:09 +0200

    add factory fn

--- src/ServiceContentRoot/ServiceContentRoot.cs
+++ src/ServiceContentRoot/ServiceContentRoot.cs
@@ -2,10 +2,13 @@ using System;
 using System.Diagnostics;
 using System.IO;
 
-namespace NetExtensions.ServiceContentRoot
+namespace NetExtensions
 {
     public class ServiceContentRoot
     {
-        public string GetContentRoot() => Process.GetCurrentProcess().MainModule != null ? Path.GetDirectoryName(Process.GetCurrentProcess().MainModule.FileName) : throw new ApplicationException("processModule is not found");
+        public static string GetContentRoot() =>
+            Process.GetCurrentProcess().MainModule != null
+                ? Path.GetDirectoryName(Process.GetCurrentProcess().MainModule.FileName)
+                : throw new ApplicationException("processModule is not found");
     }
 }
